8d6010b05b16cc29c8da7ba150996198394a2110
Nonamechick <otajonovxasan350@gmail.com>
2025-05-08 22:56:05 +0500

    little changes

--- intro.ts
+++ intro.ts
@@ -31,3 +31,58 @@ heros.map((hero):string => {
 function consoleError(errmsg: string): void {
     console.log(errmsg);
 }
+
+type User = {
+    readonly _id: string
+    name: string
+    email: string
+    isActive: boolean
+    credcardDetails?: number
+}
+function createUser(user: User) {
+    return {name: "", email:"", isActive: true};
+}
+
+createUser({name: "Kotik", email:"", isActive: true, _id: ""})
+
+let myUser: User = {
+    _id: "312",
+    name: "fsdf",
+    email: 'fdsfsdf',
+    isActive: false
+}
+myUser.email = "fsdfsfd"
+
+const s: string[] = []
+const her: Array<number> = []
+
+
+her.push(3)
+s.push('kotil')
+
+// UNION TYPES 
+let score: number | string = 33
+score = 455
+score = '4234'
+
+type User1 = {
+    name: string;
+    id: number
+}
+type Admin = {
+    name: string;
+    id: number
+}
+let kha: User1 | Admin ={name: "jal",id: 3423}
+
+//array
+const data: (string | number)[] = ['21','22',23] // or do any[]
+// const data1: string[] | number[] = ['32','234',423] not allowed in ts
+
+// TUPLES
+let tUser: [string, number, boolean] 
+
+tUser = ['fds',123,true] // order matters
+
+
+// ENUMS
